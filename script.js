19f343e05695f79bc331b7a67eb5eaebdfa8f641
shubhankgoswami <goswami.shubhank@bcg.com>
2026-04-20 18:06:40 -0700

    Add Episode 11: audio_magic_eraser

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // To add a new episode, just add a new object to the top of this array!
     const episodes = [
         {
+            title: "Episode 11: audio_magic_eraser",
+            date: "Apr 20, 2026",
+            fileUrl: "WeekOfApr20.wav"
+        },
+        {
             title: "Episode 10: Managing The Agentic Shadow Org",
             date: "Apr 19, 2026",
             fileUrl: "WeekOfApr19.wav"
