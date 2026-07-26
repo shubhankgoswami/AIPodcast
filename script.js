5caf7162bfedc2bba26304422a8fcef3afcb29fc
shubhankgoswami <goswami.shubhank@bcg.com>
2026-07-26 16:06:29 -0700

    Add Episode 24: audio_magic_eraser

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // To add a new episode, just add a new object to the top of this array!
     const episodes = [
         {
+            title: "Episode 24: audio_magic_eraser",
+            date: "Jul 26, 2026",
+            fileUrl: "WeekOfJul26.wav"
+        },
+        {
             title: "Episode 23: audio_magic_eraser",
             date: "Jul 19, 2026",
             fileUrl: "WeekOfJul19.wav"
